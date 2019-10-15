8009eb38ec82445670e344866f8a4d00d328afd9
oberdan <oberdan@craos.net>
2019-10-14 21:25:29 -0300

    Atualização ddl do banco de dados

--- public/ddl/dump.sh
+++ public/ddl/dump.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rm /Applications/lampp/apache2/htdocs/verticals/dump.sql
-/Applications/pgAdmin\ 4.app/Contents/SharedSupport/pg_dump --file "/Applications/lampp/apache2/htdocs/portal-verticals/public/ddl/dump.sql" --host "localhost" --port "5432" --username "postgres" --no-password --verbose --role "postgres" --format=p --schema-only --encoding "UTF8" "vrtc"
+/Applications/pgAdmin\ 4.app/Contents/SharedSupport/pg_dump --file "/Applications/lampp/apache2/htdocs/portal-verticals/public/ddl/dump.sql" --host "localhost" --port "5432" --username "postgres" --no-password --verbose --role "postgres" --format=p --schema-only --encoding "UTF8" "svl"
