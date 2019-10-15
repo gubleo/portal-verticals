2e8f5f78b3b029849a25e2a581e93246bdaac4de
oberdan <oberdan@craos.net>
2019-10-15 09:48:19 -0300

    Correção do local de saída do dump

--- ddl/dump.sh
+++ ddl/dump.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+echo $PWD
+
 rm /Applications/lampp/apache2/htdocs/verticals/dump.sql
-/Applications/pgAdmin\ 4.app/Contents/SharedSupport/pg_dump --file "/Applications/lampp/apache2/htdocs/portal-verticals/public/ddl/dump.sql" --host "localhost" --port "5432" --username "postgres" --no-password --verbose --role "postgres" --format=p --schema-only --encoding "UTF8" "svl"
+/Applications/pgAdmin\ 4.app/Contents/SharedSupport/pg_dump --file $PWD"/dump.sql" --host "localhost" --port "5432" --username "postgres" --no-password --verbose --role "postgres" --format=p --schema-only --encoding "UTF8" "svl"
